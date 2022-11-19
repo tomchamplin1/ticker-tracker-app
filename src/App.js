a9c0be83cd701577d510ef09c521b0685243d929
Tom Champlin <64120886+tomchamplin1@users.noreply.github.com>
2022-11-19 17:44:37 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-4xl">CREATE REACT APP WITH TAILWIND BOILERPLATE</div>
+  );
+}
+
+export default App;
