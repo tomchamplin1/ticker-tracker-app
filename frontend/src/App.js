e9b1b5530e815830e0c81b30a777daf99b35119d
Tom Champlin <64120886+tomchamplin1@users.noreply.github.com>
2022-11-19 22:48:10 -0600

    main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,12 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Entries from "./components/Entries";
-import Modal from "./components/Modal";
 
 function App() {
   return (
     <div>
       <Header />
       <Entries />
-      <Modal />
     </div>
   );
 }
